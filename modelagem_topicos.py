fe8609c12fb6c6185be7e8c1f9dc90569f96c388
Luísa F. Coelho <87907716+Luisa-Coelho@users.noreply.github.com>
2023-12-07 15:12:27 -0300

    new tfidf modelagem de topicos rede cegonha

--- modelagem_topicos.py
+++ modelagem_topicos.py
@@ -8,6 +8,9 @@ import nltk
 from nltk.corpus import stopwords
 import re
 import string
+import spacy
+from umap import UMAP
+import matplotlib.pyplot as plt
 
 # https://rustup.rs
 #https://medium.com/leti-pires/modelagem-de-tópicos-em-python-utilizando-o-modelo-de-alocação-latente-de-dirichlet-lda-3276a469f421
@@ -18,9 +21,13 @@ import string
 #  journal={arXiv preprint arXiv:2203.05794},
 #  year={2022}
 #}
+
 nltk.download('punkt')
 nltk.download('stopwords')
- 
+
+
+nlp = spacy.load("pt_core_news_sm")
+
 def extract_text(pdf_path):
     text = ""
     with open(pdf_path, "rb") as pdf_file:
@@ -32,39 +39,52 @@ def extract_text(pdf_path):
 def preprocess(docs):
     new_doc = []
     for doc in docs:
-        tokens = nltk.word_tokenize(re.sub(f"[{re.escape(string.punctuation)}]", ' ', doc.lower()))
-        tokens_hifens = nltk.word_tokenize(re.sub(f"[{re.escape(string.punctuation)}]", '-', doc.lower()))
-        hifens_filtered = [w for w in tokens_hifens if (len(w) > 1) and (w not in stopwords_set)]
-        filtered_tokens = [w for w in tokens if (len(w) > 1) and (w not in stopwords_set)]
-        #new_doc.extend(filtered_tokens)
-        new_doc.extend(hifens_filtered)
-        ## add feature para não separar palavras com hífen
+        tokens = nltk.word_tokenize(re.sub(f"[{re.escape(string.punctuation)}]", '', doc.lower()))
+        tokens_hifens = nltk.word_tokenize(re.sub(f"[{re.escape(string.punctuation)}]", '-', ' '.join(tokens)))
+        tokens_numeros = nltk.word_tokenize(re.sub(r"\\d", '', re.sub(f"[{re.escape(string.punctuation)}]", '', ' '.join(tokens_hifens))))
+        tokens_filtered = [w for w in tokens_numeros if (len(w) > 1) and (w not in stopwords_set)]
+
+        #lematizacao no spacy
+        tokens_str = ' '.join(tokens_filtered)
+        doc = nlp(tokens_str)
+        tokens_lemma = [token.lemma_ for token in doc if token.pos_ == 'NOUN']
+        
+        new_doc.extend(tokens_lemma)
+
     return new_doc
 
 pt_stopwords = stopwords.words('portuguese')
 new_stopwords = ['anexo', 'parágrafo', 'após', 'meio', 'incluindo', 'quais',
-                  'outros', 'âmbito', 'diretrizes', 'rede', 'investimentos', 'investimento']
+                  'outros', 'âmbito', 'diretrizes', 'rede', 'investimentos', 'investimento', 'ii', 'iv', 'vi', 'aih',
+                  'realizam', 'sobre', 'ão', 'desde', 'onde', 'jas', 'primeira', 'segunda' 'terceira', 'iii', 'uma', 'duas', 'dois',
+                  'n°', 'março', 'fevereiro', 'janeiro', 'abril', 'dezembro', 'julho', '°', 'art.', 'art°']
 stopwords_set = set(pt_stopwords + [word.lower() for word in new_stopwords])
 doc = preprocess(extract_text('./raw_data/portaria_rede_cegonha.pdf'))
 
 #https://www.sbert.net/docs/pretrained_models.html
+umap_model1 = UMAP(n_neighbors=15, n_components=5, 
+                  min_dist=0.0, metric='cosine', random_state=42)
+
 topic_model_emb = BERTopic(language='portuguese', embedding_model='multi-qa-MiniLM-L6-cos-v1',
-                            vectorizer_model=CountVectorizer(ngram_range=(1, 1)), top_n_words=10)
+                            vectorizer_model=CountVectorizer(ngram_range=(1, 1)), top_n_words=8, umap_model = umap_model1)
 topics_emb, probs_emb = topic_model_emb.fit_transform(doc)
 
 #https://maartengr.github.io/BERTopic/getting_started/ctfidf/ctfidf.html
+
 topic_model_tfidf = BERTopic(language='portuguese', ctfidf_model=ClassTfidfTransformer(),
-                              vectorizer_model=CountVectorizer(ngram_range=(1, 1)), top_n_words=10)
+                              vectorizer_model=CountVectorizer(ngram_range=(1, 1)), top_n_words=8, umap_model = umap_model1)
 topics_tfidf, probs_emb = topic_model_tfidf.fit_transform(doc)
 
 #https://maartengr.github.io/BERTopic/getting_started/visualization/visualization.html#visualize-documents
 # Modelagem - Embedings
-fig = topic_model_emb.visualize_barchart()
+fig = topic_model_emb.visualize_barchart(n_words = 10)
 fig.write_html("./images/emb1.html")
 
-#Modelagem TFIDF
-#topic_model_tfidf.visualize_documents()
-fig = topic_model_tfidf.visualize_barchart()
+# Modelagem TFIDF
+#fig = topic_model_tfidf.visualize_documents()
+#fig.write_html("./images/visualize.html")
+
+fig = topic_model_tfidf.visualize_barchart(n_words = 10)
 fig.write_html("./images/tfidf1.html")
 #topic_model.get_document_info(docs)
 
